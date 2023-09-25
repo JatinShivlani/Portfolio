864352227e8346c2777631d5e028489926a66a14
JatinShivlani <137112131+JatinShivlani@users.noreply.github.com>
2023-09-25 20:10:18 +0530

    Update Tech.jsx

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
-
+import { motion } from "framer-motion";
+import { fadeIn} from "../utils/motion";
 const Tech = () => {
   return (
     <div className='flex flex-row flex-wrap justify-center gap-10'>
-      {technologies.map((technology) => (
+      {technologies.map((technology,index) => (
+        <motion.div variants={fadeIn("up", "spring", index * 0.2, 1)}>
         <div className='w-28 h-28' key={technology.name}>
-          <img src={technology.image} style={{mixBlendMode:'lighter'}} alt={technology.name} />
+          <img src={technology.image} alt={technology.name} />
         </div>
+        </motion.div>
       ))}
     </div>
   );
