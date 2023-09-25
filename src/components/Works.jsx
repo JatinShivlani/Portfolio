0ce929dce2528fe2dfb043ff8e0f0d212757559a
JatinShivlani <137112131+JatinShivlani@users.noreply.github.com>
2023-09-25 19:16:19 +0530

    Update Works.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Tilt} from "react-tilt";
+import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
@@ -8,14 +8,7 @@ import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 
-const ProjectCard = ({
-  index,
-  name,
-  description,
-  tags,
-  image,
-  source_code_link,
-}) => {
+const ProjectCard = ({ index, name, description, tags, image, source_code_link }) => {
   return (
     <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
       <Tilt
@@ -24,41 +17,34 @@ const ProjectCard = ({
           scale: 1,
           speed: 450,
         }}
-        className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
+        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
       >
-        <div className='relative w-full h-[230px]'>
-          <img
-            src={image}
-            alt='project_image'
-            className='w-full h-full object-cover rounded-2xl'
-          />
+        <div className="relative w-full h-[230px]">
+          <img src={image} alt="project_image" className="w-full h-full object-cover rounded-2xl" />
 
-          <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
+          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
             <div
               onClick={() => window.open(source_code_link, "_blank")}
-              className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
             >
               <img
                 src={github}
-                alt='source code'
-                className='w-1/2 h-1/2 object-contain'
-                style={{filter: "invert(1)"}}
+                alt="source code"
+                className="w-1/2 h-1/2 object-contain"
+                style={{ filter: "invert(1)" }}
               />
             </div>
           </div>
         </div>
 
-        <div className='mt-5'>
-          <h3 className='text-white font-bold text-[24px]'>{name}</h3>
-          <p className='mt-2 text-secondary text-[14px]'>{description}</p>
+        <div className="mt-5">
+          <h3 className="text-white font-bold text-[24px]">{name}</h3>
+          <p className="mt-2 text-secondary text-[14px]">{description}</p>
         </div>
 
-        <div className='mt-4 flex flex-wrap gap-2'>
+        <div className="mt-4 flex flex-wrap gap-2">
           {tags.map((tag) => (
-            <p
-              key={`${name}-${tag.name}`}
-              className={`text-[14px] ${tag.color}`}
-            >
+            <p key={`${name}-${tag.name}`} className={`text-[14px] ${tag.color}`}>
               #{tag.name}
             </p>
           ))}
@@ -71,20 +57,30 @@ const ProjectCard = ({
 const Works = () => {
   return (
     <>
-     
+      <motion.div variants={textVariant()}>
         <p className={`${styles.sectionSubText} `}>My work</p>
         <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
-     
+      </motion.div>
 
-      <div className='w-full flex mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'>
-        
-         
-      
-          My web development portfolio is a testament to my dedication to creating meaningful and impactful digital experiences. I've had the pleasure of working on an array of projects, each with its unique challenges and opportunities. From crafting visually engaging websites to developing robust web applications, my work reflects a passion for innovation and problem-solving. Dive into these projects to see how I've translated ideas into functional, user-friendly, and aesthetically pleasing web solutions. <br /> More projects on My <a href="https://github.com/JatinShivlani" style={{textDecoration:'underline'}}>GitHub</a>
-       
+      <div className="w-full flex">
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        >
+          My web development portfolio is a testament to my dedication to creating meaningful and
+          impactful digital experiences. I've had the pleasure of working on an array of projects,
+          each with its unique challenges and opportunities. From crafting visually engaging
+          websites to developing robust web applications, my work reflects a passion for innovation
+          and problem-solving. Dive into these projects to see how I've translated ideas into
+          functional, user-friendly, and aesthetically pleasing web solutions. <br /> More projects
+          on My{" "}
+          <a href="https://github.com/JatinShivlani" style={{ textDecoration: "underline" }}>
+            GitHub
+          </a>
+        </motion.p>
       </div>
 
-      <div className='mt-20 flex flex-wrap gap-7 justify-center md:justify-start'>
+      <div className="mt-20 flex flex-wrap gap-7 justify-center md:justify-start">
         {projects.map((project, index) => (
           <ProjectCard key={`project-${index}`} index={index} {...project} />
         ))}
